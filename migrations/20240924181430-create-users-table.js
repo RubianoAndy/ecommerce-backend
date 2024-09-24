872d07c718c2eb3dcc61c47c6930fd3bd0a954e7
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-09-24 13:27:53 -0500

    feat: adding users and profiles tables migrations

---
+++ migrations/20240924181430-create-users-table.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false, // Obligatorio
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Establece la fecha actual por defecto
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Establece la fecha actual por defecto
+      },
+      activated: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      email: {
+        type: Sequelize.STRING(100), // Tamaño máximo de 100
+        unique: true,
+        allowNull: false  // Obligatorio
+      },
+      password: {
+        type: Sequelize.STRING(30), // Puedes especificar un tamaño si lo deseas
+        allowNull: false  // Obligatorio
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ migrations/20240924181824-create-profiles-table.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('profiles', {
+      id: {
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Establece la fecha actual por defecto
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Establece la fecha actual por defecto
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      name_1: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      },
+      name_2: {
+        type: Sequelize.STRING(20),
+        allowNull: true
+      },
+      lastname_1: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+      },
+      lastname_2: {
+        type: Sequelize.STRING(20),
+        allowNull: true
+      },
+      identification_number: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      prefix: {
+        type: Sequelize.STRING(4),
+        allowNull: true
+      },
+      mobile: {
+        type: Sequelize.STRING(15),
+        allowNull: true
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('profiles');
+  }
+};
