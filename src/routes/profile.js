67814d139f6c0bd016321085e29ab214a9248511
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-11-26 14:40:40 -0500

    feat: adding update profile request

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -58,4 +58,49 @@ router.get('/profile', authMiddleware, async (request, response) => {
     }
 });
 
+router.patch('/update-profile', authMiddleware, async (request, response) => {
+    const userId = request.accessTokenDecoded.id;
+
+    const { name_2, lastname_2, dniType, dni, prefix, mobile } = request.body;
+
+    if (!prefix || !mobile)
+        return response.status(400).json({ message: 'Los campos obligatorios están incompletos' });
+
+    try {
+        const user = await User.findOne({ where: { id: userId } });
+        if (!user)
+            return response.status(404).json({ message: 'No existe usuario asociado' });
+        
+        if (!user.activated)
+            return response.status(403).json({ message: 'Usuario inactivo' });
+
+        const profile = await Profile.findOne({ where: { userId } });
+        if (!profile)
+            return response.status(404).json({ message: 'Perfil no encontrado' });
+
+        profile.prefix = prefix;
+        profile.mobile = mobile;
+
+        if(dniType)
+            profile.dniType = dniType;
+        
+        if (dni)
+            profile.dni = dni;
+
+        if (name_2)
+            profile.name_2 = name_2;
+        
+        if (lastname_2)
+            profile.lastname_2 = lastname_2;
+
+        await profile.save();
+
+        return response.status(200).json({ message: 'Perfil actualizado correctamente' });
+
+    } catch (error) {
+        logger.error(`Error al actualizar la información del perfil: ${error.message}`);
+        return response.status(500).json({ message: 'Error al actualizar la información del perfil', details: error.message });
+    }
+});
+
 module.exports = router;
