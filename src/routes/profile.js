27d49ecaaa1232df33bbd4caad2aedd5a625cc53
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-11-27 10:40:11 -0500

    feat: adding put and get profile from SuperAdmin

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,10 +2,14 @@
 
 const express = require('express');
 const winston = require('winston');
+const validator = require('validator');
 require('dotenv').config();
 
 const { User, Profile } = require('../../models');
 const authMiddleware = require('../middlewares/auth-middleware');
+const roleMiddleware = require('../middlewares/role-middleware');
+
+const SUPER_ADMIN = Number(process.env.SUPER_ADMIN);
 
 const logger = winston.createLogger({
     level: 'error',
@@ -46,7 +50,7 @@ router.get('/profile', authMiddleware, async (request, response) => {
             prefix: profile.prefix,
             mobile: profile.mobile,
             email: user.email,
-            roleId: user.roleId,
+            // roleId: user.roleId,
 
             message: 'Perfil cargado exitosamente',
         };
@@ -58,7 +62,7 @@ router.get('/profile', authMiddleware, async (request, response) => {
     }
 });
 
-router.patch('/update-profile', authMiddleware, async (request, response) => {
+router.put('/update-profile', authMiddleware, async (request, response) => {
     const userId = request.accessTokenDecoded.id;
 
     const { name_2, lastname_2, dniType, dni, prefix, mobile } = request.body;
@@ -94,6 +98,99 @@ router.patch('/update-profile', authMiddleware, async (request, response) => {
             profile.lastname_2 = lastname_2;
 
         await profile.save();
+
+        return response.status(200).json({ message: 'Perfil actualizado correctamente' });
+
+    } catch (error) {
+        logger.error(`Error al actualizar la información del perfil: ${error.message}`);
+        return response.status(500).json({ message: 'Error al actualizar la información del perfil', details: error.message });
+    }
+});
+
+router.get('/profile/:userId', authMiddleware, roleMiddleware([ SUPER_ADMIN ]), async (request, response) => {
+    const userId = Number(request.params.userId);
+    if (isNaN(userId) || userId <= 0)
+        return response.status(400).json({ message: 'ID de usuario inválido' });
+    
+    try {
+        const user = await User.findOne({ where: { id: userId } });
+        if (!user)
+            return response.status(404).json({ message: 'No existe usuario asociado' });
+        
+        // if (!user.activated)
+        //     return response.status(403).json({ message: 'Usuario inactivo' });
+
+        const profile = await Profile.findOne({ where: { userId } });
+        if (!profile)
+            return response.status(404).json({ message: 'Perfil no encontrado' });
+
+        const result = {
+            id: profile.id,
+            name_1: profile.name_1,
+            name_2: profile.name_2,
+            lastname_1: profile.lastname_1,
+            lastname_2: profile.lastname_2,
+            dniType: profile.dniType,
+            dni: profile.dni,
+            prefix: profile.prefix,
+            mobile: profile.mobile,
+            email: user.email,
+            // roleId: user.roleId,
+
+            message: 'Perfil cargado exitosamente',
+        };
+        
+        return response.status(200).json(result);
+    } catch (error) {
+        logger.error(`Error al obtener el perfil: ${error.message}`);
+        return response.status(500).json({ message: 'Error al obtener el perfil', details: error.message });
+    }
+});
+
+router.put('/update-profile/:userId', authMiddleware, roleMiddleware([ SUPER_ADMIN ]), async (request, response) => {
+    const userId = request.params.userId;
+    if (isNaN(userId) || userId <= 0)
+        return response.status(400).json({ message: 'ID de usuario inválido' });
+
+    const { name_1, name_2, lastname_1, lastname_2, dniType, dni, prefix, mobile, email } = request.body;
+    if (!name_1 || !lastname_1 || !dniType || !dni || !prefix || !mobile || !email)
+        return response.status(400).json({ message: 'Los campos obligatorios están incompletos' });
+
+    if (!validator.isEmail(email))
+        return response.status(400).json({ message: 'El formato del correo electrónico no es válido' });
+
+    try {
+        const user = await User.findOne({ where: { id: userId } });
+        if (!user)
+            return response.status(404).json({ message: 'No existe usuario asociado' });
+        
+        // if (!user.activated)
+        //     return response.status(403).json({ message: 'Usuario inactivo' });
+
+        const profile = await Profile.findOne({ where: { userId } });
+        if (!profile)
+            return response.status(404).json({ message: 'Perfil no encontrado' });
+
+        profile.name_1 = name_1;
+        profile.lastname_1 = lastname_1;
+        profile.dniType = dniType;
+        profile.dni = dni;
+        profile.prefix = prefix;
+        profile.mobile = mobile;
+
+        user.email = email;
+
+        if (name_2)
+            profile.name_2 = name_2;
+        
+        if (lastname_2)
+            profile.lastname_2 = lastname_2;
+
+        await Promise.all([
+            profile.save(),
+            user.save(),
+
+        ]);
 
         return response.status(200).json({ message: 'Perfil actualizado correctamente' });
 
