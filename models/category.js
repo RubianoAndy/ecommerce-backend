4b87738ad0a366bccd9854bda870e620b509b535
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-12-14 16:51:17 -0500

    feat: adding Category model

---
+++ models/category.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.BIGINT,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            unique: true,
+            allowNull: false,
+        },
+    }, {
+        tableName: 'categories',
+        timestamps: true,   // Habilita automáticamente createdAt y updatedAt
+        paranoid: true,     // Habilita automáticamente soft delete (deletedAt)
+    });
+
+
+    return Category;
+};
