e0d097eed40d7df34e204664674db5bd10b66fe6
Andy Rubiano™ <AR.Andy.Rubiano@gmail.com>
2024-12-27 17:28:36 -0500

    feat: adding correspondence notification email

--- src/routes/correspondences.js
+++ src/routes/correspondences.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const express = require('express');
-const nodemailer = require('nodemailer');
-const winston = require('winston');
 const fs = require('fs').promises;
 const path = require('path');
 require('dotenv').config();
@@ -13,7 +11,6 @@ const transporter = require('../config/transporter');
 const { User, Profile, Correspondence } = require('../../models');
 const authMiddleware = require('../middlewares/auth-middleware');
 const roleMiddleware = require('../middlewares/role-middleware');
-const { where } = require('sequelize');
 
 const SUPER_ADMIN = Number(process.env.SUPER_ADMIN);
 
@@ -89,30 +86,27 @@ router.post('/correspondence', authMiddleware, async (request, response) => {
                     observations,
                 });
 
-                /* const filePath = path.join(__dirname, '../utils/email/activate-account.html');
+                const filePath = path.join(__dirname, '../utils/email/correspondence-notification.html');
 
                 try {
                     const htmlContent = await fs.readFile(filePath, 'utf-8');
 
                     const personalizedHtml = htmlContent
-                        .replace('{{ name_1 }}', name_1)
-                        .replace('{{ lastname_1 }}', lastname_1)
-                        .replace('{{ activateUrl }}', activateUrl)
-                        .replace('{{ apiURL }}', process.env.API_URL)
-                        .replace('{{ support_email }}', process.env.SUPPORT_EMAIL)
-                        .replace('{{ support_number }}', process.env.SUPPORT_WHATSAPP);
+                        .replace('{{ name_1 }}', profile.name_1)
+                        .replace('{{ lastname_1 }}', profile.lastname_1)
+                        .replace('{{ apiURL }}', process.env.API_URL);
 
                     const mailContent = {
                         // from: `"${name}" <${email}>`,
-                        to: email,
-                        subject: '¡Activa tu cuenta!',
+                        to: user.email,
+                        subject: '¡Información de envío registrada satisfactoriamente!',
                         html: personalizedHtml
                     };
 
                     await transporter.sendMail(mailContent);
                 } catch (error) {
                     logger.error(`Error al enviar el correo: ${error.message}`);
-                } */
+                }
 
                 correspondence = await Correspondence.findOne({ where: { profileId: profile.id } });
                 if(!correspondence)
@@ -246,30 +240,27 @@ router.post('/correspondence/:userId', authMiddleware, roleMiddleware([ SUPER_AD
                     observations,
                 });
 
-                /* const filePath = path.join(__dirname, '../utils/email/activate-account.html');
+                const filePath = path.join(__dirname, '../utils/email/correspondence-notification.html');
 
                 try {
                     const htmlContent = await fs.readFile(filePath, 'utf-8');
 
                     const personalizedHtml = htmlContent
-                        .replace('{{ name_1 }}', name_1)
-                        .replace('{{ lastname_1 }}', lastname_1)
-                        .replace('{{ activateUrl }}', activateUrl)
-                        .replace('{{ apiURL }}', process.env.API_URL)
-                        .replace('{{ support_email }}', process.env.SUPPORT_EMAIL)
-                        .replace('{{ support_number }}', process.env.SUPPORT_WHATSAPP);
+                        .replace('{{ name_1 }}', profile.name_1)
+                        .replace('{{ lastname_1 }}', profile.lastname_1)
+                        .replace('{{ apiURL }}', process.env.API_URL);
 
                     const mailContent = {
                         // from: `"${name}" <${email}>`,
-                        to: email,
-                        subject: '¡Activa tu cuenta!',
+                        to: user.email,
+                        subject: '¡Información de envío registrada satisfactoriamente!',
                         html: personalizedHtml
                     };
 
                     await transporter.sendMail(mailContent);
                 } catch (error) {
                     logger.error(`Error al enviar el correo: ${error.message}`);
-                } */
+                }
 
                 correspondence = await Correspondence.findOne({ where: { profileId: profile.id } });
                 if(!correspondence)
